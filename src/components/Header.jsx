c557a1e98ddf34a2fc2906b54b8ea1a3a5ab690d
Arthur Telles <arthurtelles19@gmail.com>
2024-06-16 18:07:18 -0300

    logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import Logo from '../img/logo.png'
 
 const Header = () => {
     return (
         <div className='container  bg-transparent text-white'>
             <div className='navBar flex justify-between pt-8 pb-4 max-w-full px-28' >
-                <div className='logo'>
-                    <h1>LOGO</h1>
+                <div className='logo flex'>
+                    <img src={Logo}  alt="" className='w-24 h-24 ' />
+
+                    <div className='mt-6 pl-4'>
+                        <a href="" className='font-Sora-medium hover:text-[#8750f7] '>arthur.telles@ufvjm.edu.br</a>
+                    </div>
+
                 </div>
 
-                <div className=''>
+                <div className='mt-4'>
                     <ul className='flex gap-12'>
                         <li className=''>
                             <a className='navBar-Link text-base font-Sora-medium ' href="">Work</a>
